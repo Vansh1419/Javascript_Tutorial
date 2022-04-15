1bb673fcf639f422e22d63145b17dbcb7eddf557
Vansh1419 <vanshjain1419@gmail.com>
2022-04-15 16:09:01 +0530

    file107.js completed

---
+++ js_files/DOM_Learning/file107.js
@@ -0,0 +1,6 @@
+// Change style of elements
+const mainHeading = document.querySelector("div.headline h2")
+// console.log(mainHeading.style)
+mainHeading.style.color = "blue";
+// mainHeading.style.backgroundColor = "pink";
+// mainHeading.style.background-color = "pink";// it is not possible in javaScript
