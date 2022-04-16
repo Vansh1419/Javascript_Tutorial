1a06bb62d22fced5cf830f6ed3e4cfd714e8a68f
Vansh1419 <vanshjain1419@gmail.com>
2022-04-16 13:27:40 +0530

    file113.js completed

---
+++ js_files/DOM_Learning/file113.js
@@ -0,0 +1,8 @@
+// classList ,add and remove, toggle classes
+const sectionToDo = document.querySelector(".section-todo");
+// console.log(sectionToDo.classList);
+// sectionToDo.classList.add("bg-dark");
+// sectionToDo.classList.remove("container");
+// const check = sectionToDo.classList.contains("container");
+console.log(check);
+sectionToDo.classList.toggle("bg-dark");// if present then remove or vice versa
